28b5f2690dc77ae61398673670169a51cc17f917
junggri <gkgkgk6933@gmail.com>
2022-04-29 09:44:56 +0900

    feat: complete 90%

--- src/observer.ts
+++ src/observer.ts
@@ -30,19 +30,36 @@ export function observer(obj: { [key: string]: any }) {
 
 //얕은 복사는 참조값이 복사되는거 //깊은복사는 참조값이 모두 끊어지고 값이 복시되는 것
 function checkIsSame(previous, current): boolean {
-  if (previous instanceof Object && current instanceof Object) {
-    if (previous instanceof Array && current instanceof Array) {
-
-    } else {
-
-    }
-
+  if (previous instanceof Array && current instanceof Array) {
+    return equalArray(previous, current);
+  } else if (previous instanceof Object && current instanceof Object) {
+    return equalObject(previous, current);
   } else {
-    return previous === current
+    return previous === current;
   }
 }
 
 
-function arrayShallowCompare() {
+function equalArray(pre, cur) {
 
+  if (pre.length !== cur.length) return false;
+
+  for (let i = 0; i < cur.length; i++) {
+    if (cur[i] instanceof Array && pre[i] instanceof Array) {
+      equalArray(pre[i], cur[i]);
+    }
+    if (pre[i] !== cur[i]) return false;
+  }
+  return true;
+}
+
+
+function equalObject(pre, cur) {
+  for (let key in pre) {
+    if (pre[key] instanceof Object) {
+      equalObject(pre[key], cur[key]);
+    }
+    if (pre[key] !== cur[key]) return false;
+  }
+  return true;
 }
